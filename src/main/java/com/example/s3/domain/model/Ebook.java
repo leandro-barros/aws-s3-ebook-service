a77e5677f1d30b06b98ffa35965f9c25ebdc71ed
Leandro Barros <leandroedbarros>
2023-02-16 22:51:52 -0300

    feat: Creating dtos class

--- src/main/java/com/example/s3/domain/model/Ebook.java
+++ src/main/java/com/example/s3/domain/model/Ebook.java
@@ -11,9 +11,8 @@ import java.util.Objects;
 import java.util.UUID;
 
 @Getter
-@Setter
 @Builder
-@EqualsAndHashCode
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Ebook {
 
@@ -22,6 +21,7 @@ public class Ebook {
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(name = "id", columnDefinition = "char(36)")
     @Type(type = "org.hibernate.type.UUIDCharType")
+    @EqualsAndHashCode.Include
     private UUID id;
 
     @CreationTimestamp
@@ -31,22 +31,38 @@ public class Ebook {
 
     private String autor;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    private FileReference cover;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private FileReference attachment;
+
     protected Ebook() {
         //JPA/Hibernate
     }
 
     public Ebook(UUID id, OffsetDateTime createdAt,
-                 String title, String autor) {
+                 String title, String autor,
+                 FileReference cover, FileReference attachment) {
         Objects.requireNonNull(title);
         Objects.requireNonNull(autor);
+        Objects.requireNonNull(cover);
+        Objects.requireNonNull(attachment);
+
         this.id = id;
         this.createdAt = createdAt;
         this.title = title;
         this.autor = autor;
+        this.cover = cover;
+        this.attachment = attachment;
     }
 
     public void update(Ebook ebookUpdated) {
+        Objects.requireNonNull(ebookUpdated);
         this.title = ebookUpdated.title;
         this.autor = ebookUpdated.autor;
+        this.cover = ebookUpdated.cover;
+        this.attachment = ebookUpdated.attachment;
     }
+
 }
