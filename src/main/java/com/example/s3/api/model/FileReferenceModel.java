973ad4fdd6249e0962915ddae26aea7fbe597fa8
Leandro Barros <leandroedbarros>
2023-02-21 21:40:12 -0300

    feat: Changing FileReferenceModel class

---
+++ src/main/java/com/example/s3/api/model/FileReferenceModel.java
@@ -0,0 +1,24 @@
+package com.example.s3.api.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
+public class FileReferenceModel {
+
+    private UUID id;
+
+    private boolean publicAccessible;
+
+    private String name;
+
+    private String contentType;
+
+    private Long contentLength;
+
+    private String downloadUrl;
+
+}
